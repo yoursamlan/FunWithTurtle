f18340ecd1a5c8a9bd68899a9b96133ae2267bc8
Amlan Saha Kundu <skamlan99@gmail.com>
2019-06-29 19:25:46 +0530

    Create Graph Paper.py

---
+++ Graph Paper.py
@@ -0,0 +1,13 @@
+#It's the funniest way to draw a graph paper
+import turtle
+from turtle import *
+
+for i in range (20,500,10):
+    turtle.forward(int(i))
+    turtle.right(90)
+    for j in range (20,100,10):
+        turtle.forward(int(i-j))
+        turtle.right(90)
+        for k in range (20,50,50):
+            turtle.forward(int(i+k))
+            turtle.right(90)
