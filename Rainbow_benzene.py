c85d52b6ef5283c41a5d935aaaece4783071d802
Amlan Saha Kundu <skamlan99@gmail.com>
2019-06-29 21:54:13 +0530

    Create Rainbow_benzene.py

---
+++ Rainbow_benzene.py
@@ -0,0 +1,11 @@
+import turtle 
+
+colors = ['purple', 'blue', 'green', 'orange', 'yellow','red'] 
+t = turtle.Pen() 
+turtle.bgcolor('black')
+t.speed(0)
+for x in range(360): 
+    t.pencolor(colors[x%6]) 
+    t.width(x/100 + 1) 
+    t.forward(x) 
+    t.left(59) 
